6774fc8619a935bb61e14dbc43e4fe1c4036aa94
oceanvacationsmb <oceanvacationsmb@gmail.com>
2026-03-20 00:12:41 -0400

    Update EmailJS credentials in script.js

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ let reservationsData = [];
 
 // ====== REPLACE THESE IDS WITH YOUR EMAILJS ACCOUNT'S IDS ======
 // Find these in EmailJS > Integration (user ID), Services, Templates
-const EMAILJS_USER_ID = "YOUR_EMAILJS_USER_ID";
-const EMAILJS_SERVICE_ID = "YOUR_EMAILJS_SERVICE_ID";
-const EMAILJS_TEMPLATE_ID = "YOUR_EMAILJS_TEMPLATE_ID";
+const EMAILJS_USER_ID = "ti3155";
+const EMAILJS_SERVICE_ID = "service_06c56l2";
+const EMAILJS_TEMPLATE_ID = "template_91j57r4";
 // ===============================================================
 (function(){
   emailjs.init(EMAILJS_USER_ID);
