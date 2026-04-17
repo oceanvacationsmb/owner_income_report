7dccf977a19fb43150fc5ff283807c9c8fbc4b5b
oceanvacationsmb <oceanvacationsmb@gmail.com>
2026-04-17 10:20:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -270,6 +270,16 @@ const OWNERS = {
     cleaningFee: 0,
     viewMode: "payout"
   },
+   "jeff@upstatedogtraining.com": {
+    password: "owner1111",
+    ownerName: "Jeff",
+    propertyName: "827B N.WACCAMMAW DR.",
+    postalCode: "29582",
+    pmcPercent: 12,
+    guestyApiKey: "https://report.guesty.com/apps/reservations?apiKey=fd355bb8135be1c79eb2d87583d16435c0e4e9e433b3a257c827ed24f1bfc8a2d30ee5ca78ea482ec904fafd2499cce6c57ccafe5b0594924062b3c1572a35be",
+    cleaningFee: 0,
+    viewMode: "payout"
+  },
   };
 
 applyOwnerOverridesFromStorage();
